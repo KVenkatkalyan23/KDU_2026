7abe133788d205f9ced796c3fb97c14d0793bf44
koppala venkat kalyan <koppala.kalyan@kickdrumtech.com>
2026-01-07 15:01:02 +0530

    commit

--- java-multithreading/src/main/java/org/example/Main.java
+++ java-multithreading/src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.exerciseOne.MainOne;
+import org.example.exerciseThree.MainThree;
 import org.example.exerciseTwo.MainTwo;
 
 import java.util.concurrent.ExecutionException;
@@ -8,9 +9,8 @@ import java.util.concurrent.ExecutionException;
 public class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         MainOne.run();
-
-//        MainThree.callableExample();
-//        MainThree.streamExample();
-
+        MainTwo.run();
+        MainThree.callableExample();
+        MainThree.streamExample();
     }
 }
