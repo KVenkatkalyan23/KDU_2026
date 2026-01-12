42c7943a9e813d75848e50ab709edda536e6258a
koppala venkat kalyan <koppala.kalyan@kickdrumtech.com>
2026-01-12 12:54:32 +0530

    edited constans

--- demo/src/main/java/com/example/jwt/demo/constant/Constants.java
+++ demo/src/main/java/com/example/jwt/demo/constant/Constants.java
@@ -1,6 +1,6 @@
 package com.example.jwt.demo.constant;
 
 public class Constants {
-    public static final Double PRICE = 0.25;
+    public static final Double PRICE = 2.50;
     public static final String SECRET_KEY = "my_super_secret_key_which_is_at_least_256_bits_long_123!gfghdfxdf";
 }
